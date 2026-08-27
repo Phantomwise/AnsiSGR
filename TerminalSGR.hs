c794e0eacbec50dd41d22a06bcdc762be9267e72
Phantomwise <phantomwise67@protonmail.com>
2026-08-27 16:03:43 +0200

    ♻️ Move data type and functions for the 8 basic colors into a separate module

--- TerminalSGR.hs
+++ TerminalSGR.hs
@@ -12,6 +12,8 @@ module TerminalSGR
 -- ================================================================
 
 
+import Color8
+
 import Intensity
 import Typeface
 import Underline
@@ -38,98 +40,8 @@ import Text.Read (readMaybe)
 
 
 -- ================================================================
--- TYPES
--- ================================================================
-
-
-data Color      = Black | Red | Green | Yellow | Blue | Magenta | Cyan | White
-data Brightness = Dull | Vivid
-
--- ================================================================
 -- COLOR FUNCTIONS
 -- ================================================================
-
-
-fg :: Brightness -> Color -> String
-fg Dull  Black   = "\x1b[30m"
-fg Dull  Red     = "\x1b[31m"
-fg Dull  Green   = "\x1b[32m"
-fg Dull  Yellow  = "\x1b[33m"
-fg Dull  Blue    = "\x1b[34m"
-fg Dull  Magenta = "\x1b[35m"
-fg Dull  Cyan    = "\x1b[36m"
-fg Dull  White   = "\x1b[37m"
-fg Vivid Black   = "\x1b[90m"
-fg Vivid Red     = "\x1b[91m"
-fg Vivid Green   = "\x1b[92m"
-fg Vivid Yellow  = "\x1b[93m"
-fg Vivid Blue    = "\x1b[94m"
-fg Vivid Magenta = "\x1b[95m"
-fg Vivid Cyan    = "\x1b[96m"
-fg Vivid White   = "\x1b[97m"
-
-
-fgd :: Color -> String
-fgd Black   = "\x1b[30m"
-fgd Red     = "\x1b[31m"
-fgd Green   = "\x1b[32m"
-fgd Yellow  = "\x1b[33m"
-fgd Blue    = "\x1b[34m"
-fgd Magenta = "\x1b[35m"
-fgd Cyan    = "\x1b[36m"
-fgd White   = "\x1b[37m"
-
-
-fgv :: Color -> String
-fgv Black   = "\x1b[90m"
-fgv Red     = "\x1b[91m"
-fgv Green   = "\x1b[92m"
-fgv Yellow  = "\x1b[93m"
-fgv Blue    = "\x1b[94m"
-fgv Magenta = "\x1b[95m"
-fgv Cyan    = "\x1b[96m"
-fgv White   = "\x1b[97m"
-
-
-bg :: Brightness -> Color -> String
-bg Dull Black    = "\x1b[40m"
-bg Dull Red      = "\x1b[41m"
-bg Dull Green    = "\x1b[42m"
-bg Dull Yellow   = "\x1b[43m"
-bg Dull Blue     = "\x1b[44m"
-bg Dull Magenta  = "\x1b[45m"
-bg Dull Cyan     = "\x1b[46m"
-bg Dull White    = "\x1b[47m"
-bg Vivid Black   = "\x1b[100m"
-bg Vivid Red     = "\x1b[101m"
-bg Vivid Green   = "\x1b[102m"
-bg Vivid Yellow  = "\x1b[103m"
-bg Vivid Blue    = "\x1b[104m"
-bg Vivid Magenta = "\x1b[105m"
-bg Vivid Cyan    = "\x1b[106m"
-bg Vivid White   = "\x1b[107m"
-
-
-bgd :: Color -> String
-bgd Black   = "\x1b[40m"
-bgd Red     = "\x1b[41m"
-bgd Green   = "\x1b[42m"
-bgd Yellow  = "\x1b[43m"
-bgd Blue    = "\x1b[44m"
-bgd Magenta = "\x1b[45m"
-bgd Cyan    = "\x1b[46m"
-bgd White   = "\x1b[47m"
-
-
-bgv :: Color -> String
-bgv Black   = "\x1b[100m"
-bgv Red     = "\x1b[101m"
-bgv Green   = "\x1b[102m"
-bgv Yellow  = "\x1b[103m"
-bgv Blue    = "\x1b[104m"
-bgv Magenta = "\x1b[105m"
-bgv Cyan    = "\x1b[106m"
-bgv White   = "\x1b[107m"
 
 
 fg256 :: Word8 -> String
@@ -180,11 +92,6 @@ hexToWord :: String -> Word8
 hexToWord hex = case readMaybe ("0x" ++ hex) of
     Just x -> x
     Nothing -> error "Invalid hex value: Must be in the range 00-ff"
-
-
--- ================================================================
--- STYLE FUNCTIONS
--- ================================================================
 
 
 -- ================================================================
