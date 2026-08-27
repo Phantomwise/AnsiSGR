8eefd368d561e70d82b6c9f4bd5b303bd8e8f687
Phantomwise <phantomwise67@protonmail.com>
2026-08-27 16:09:12 +0200

    ♻️ Move functions for the 256 colors into a separate module

--- TerminalSGR.hs
+++ TerminalSGR.hs
@@ -13,6 +13,7 @@ module TerminalSGR
 
 
 import Color8
+import Color256
 
 import Intensity
 import Typeface
@@ -44,20 +45,11 @@ import Text.Read (readMaybe)
 -- ================================================================
 
 
-fg256 :: Word8 -> String
-fg256 arg = "\x1b[38;5;" ++ show arg ++ "m"
-
 fgRGB :: Word8 -> Word8 -> Word8 -> String
 fgRGB r g b = "\x1b[38;2;" ++ (show r) ++ ";" ++ (show g) ++ ";" ++ (show b) ++ "m"
 
-bg256 :: Word8 -> String
-bg256 arg = "\x1b[48;5;" ++ show arg ++ "m"
-
 bgRGB :: Word8 -> Word8 -> Word8 -> String
 bgRGB r g b = "\x1b[48;2;" ++ (show r) ++ ";" ++ (show g) ++ ";" ++ (show b) ++ "m"
-
-ul256 :: Word8 -> String
-ul256 arg = "\x1b[58;5;" ++ show arg ++ "m"
 
 ulRGB :: Word8 -> Word8 -> Word8 -> String
 ulRGB r g b = "\x1b[58;2;" ++ (show r) ++ ";" ++ (show g) ++ ";" ++ (show b) ++ "m"
