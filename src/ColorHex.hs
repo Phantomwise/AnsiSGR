ecb2523fbe448ab8db1e27cd563be2fe96d5e39b
Phantomwise <phantomwise67@protonmail.com>
2026-08-28 04:02:53 +0200

    ✨ Add support for Hex colors starting with `#`

--- src/ColorHex.hs
+++ src/ColorHex.hs
@@ -14,25 +14,29 @@ import Text.Read (readMaybe)
 
 
 fgHex :: String -> String
-fgHex hex = case validateHex hex of
+fgHex hex = case normalizeHex hex of
     [r, g, b] -> fgRGB r g b
     _         -> error "Unreachable"
 
 bgHex :: String -> String
-bgHex hex = case validateHex hex of
+bgHex hex = case normalizeHex hex of
     [r, g, b] -> bgRGB r g b
     _         -> error "Unreachable"
 
 ulHex :: String -> String
-ulHex hex = case validateHex hex of
+ulHex hex = case normalizeHex hex of
     [r, g, b] -> ulRGB r g b
     _         -> error "Unreachable"
 
 
+normalizeHex :: String -> [Word8]
+normalizeHex ('#':rest) = validateHex rest
+normalizeHex s          = validateHex s
+
 validateHex :: String -> [Word8]
 validateHex hex
     | length hex == 6 = decomposeHex hex
-    | otherwise       = error "Invalid hex value: Must have exactly 6 characters"
+    | otherwise       = error "Invalid hex value: Must have exactly 6 characters after the optional leading `#`"
 
 decomposeHex :: String -> [Word8]
 decomposeHex (a:b:c:d:e:f:[]) = map hexToWord [[a,b], [c,d], [e,f]]
